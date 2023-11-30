3dd7e463019a17e17b2a1d81640e5d5e50679005
Jospen-wolongwo <jospen.wolongwo@hellocomputing.tech>
2023-11-30 17:08:55 +0100

    feat/initial start of the counter-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'counter-app';
+  value = 0;
+  addFunction(){
+    if(this.value < 10){
+    this.value++;
+  }
+  }
+  subtractFunction(){
+    if(this.value > 0){
+      this.value--;
+    }
+  }
+  
 }
